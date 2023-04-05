7acd91f674854fa90f39bf1ca293793a18f1c512
Jonathan Willis <Jonathan.Willis@vu.com>
2023-04-04 21:54:40 -0500

    9.4 my code

--- counter.py
+++ counter.py
@@ -2,7 +2,23 @@ def countEmail():
     # This first line is provided for you
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
+    sender_collection = dict()
     handle = open(name)
+    for line in handle:
+        if line.startswith('From '):
+            line_split = line.split()
+            sender = line_split[1]
+            sender_collection[sender] = sender_collection.get(sender, 0) + 1
+   
+    largest = 0
+    for item in sender_collection:
+        if sender_collection[item] > largest:
+            largest = sender_collection[item]
+            max_key = item
+    
+    print(max_key, sender_collection[max_key])
+
+    handle.close()
         
 
 ## if you want to test locally run > python counter.py
